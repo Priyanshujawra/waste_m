3cbd8bfe6c4fdb79f5782021ceff851863290955
Priyanshu kumar <109659772+Priyanshujawra@users.noreply.github.com>
2024-11-08 22:03:13 +0530

    Update app.js

--- waste-M-backend/app.js
+++ waste-M-backend/app.js
@@ -34,7 +34,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://hotel-backend-xi.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
